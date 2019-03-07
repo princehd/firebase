ca396f2f8f6fcbdd6c3874b2f04dd0c074734799
goehehgo <goehehgo@naver.com>
2019-03-07 13:45:01 +0900

    - doPost 메소드를 통한 patch기능 추가

--- src/main/java/servlet/Firebase01.java
+++ src/main/java/servlet/Firebase01.java
@@ -89,4 +89,37 @@ public class Firebase01 extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		setting(request);
+		
+		BufferedReader reader = request.getReader();
+		StringBuffer buffer = new StringBuffer();
+		String s;
+		while((s=reader.readLine()) != null) {
+			buffer.append(s);
+		}
+		
+		Map<String, Object> jsonMap = null;
+		try {
+			jsonMap = JacksonUtility.GET_JSON_STRING_AS_MAP(buffer.toString());
+		} catch (JacksonUtilityException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			FirebaseResponse res = firebase.patch(reqURI , jsonMap );
+			System.out.println( "\n\nResult of POST :\n" + res );
+			System.out.println("\n");
+		} catch (FirebaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (JacksonUtilityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
